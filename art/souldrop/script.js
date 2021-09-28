f2a2a45e197876def7020093e1fdcfc35fe5b8d5
imneocoder <imneocoder@gmail.com>
2021-09-28 23:02:19 +0530

    particles animation done

--- art/souldrop/script.js
+++ art/souldrop/script.js
@@ -1,93 +1,75 @@
 const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
 
-// sign
-context.fillStyle = "#2B3531";
-context.fillRect(200, 150, 200, 140);
+window.addEventListener('resize', function(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
 
-// Left Outta Ear
-context.beginPath();
-context.moveTo(275, 20);
-context.lineTo(255, 60);
-context.lineTo(295, 60);
-context.fillStyle = '#F74700';
-context.fill()
+const mouse = {
+  x: null,
+  y: null,
+}
 
-// Left Inner Ear
-context.beginPath();
-context.moveTo(275, 32);
-context.lineTo(265, 55);
-context.lineTo(285, 55);
-context.fillStyle = '#000';
-context.fill()
+canvas.addEventListener('click', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
 
-// Right Outta Ear
-context.beginPath();
-context.moveTo(325, 20);
-context.lineTo(305, 60);
-context.lineTo(345, 60);
-context.fillStyle = '#F74700';
-context.fill()
+canvas.addEventListener('mousemove', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+})
 
-// Right Inner Ear
-context.beginPath();
-context.moveTo(325, 32);
-context.lineTo(315, 55);
-context.lineTo(335, 55);
-context.fillStyle = '#000';
-context.fill()
+class Particle {
+  constructor(){
+    this.x = mouse.x;
+    this.y = mouse.y;
+    //this.x = Math.random() * canvas.width;
+    //this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+  update(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1; 
+  }
+  draw(){
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(this.x,this.y,this.size,0, Math.PI * 2);
+    ctx.fill();
+  }
+}
 
-// head
-context.beginPath();
-context.moveTo(300, 150);
-context.lineTo(240, 60);
-context.lineTo(360, 60);
-context.fillStyle = '#F74700';
-context.fill();
+function init() {
+  for(let i=0; i<1000; i++){
+    particlesArray.push(new Particle());
+  }
+}
 
-// legs
-context.fillRect(255, 290, 40, 60);
-context.fillRect(305, 290, 40, 60);
+init();
 
-// Left Foot
-context.beginPath();
-context.arc(275, 350, 20,( Math.PI/180 ) * 180, ( Math.PI/180 ) * 0);
-context.fillStyle = '#000';
-context.fill()
+function handleParticles() {
+  for(let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+    if (particlesArray[i].size <= 0.3) {
+      particlesArray.splice(i,1);
+      i--;
+    }
+  }
+}
 
-// Right Foot
-context.beginPath();
-context.arc(325, 350, 20,( Math.PI/180 ) * 180, ( Math.PI/180 ) * 0);
-context.fillStyle = '#000';
-context.fill()
+function animate() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  handleParticles();
+  requestAnimationFrame(animate);
+}
 
-// left arm
-context.beginPath();
-context.arc(180, 180, 20, 0, Math.PI * 2, true);
-context.fillStyle = '#F74700';
-context.fill();
-
-// right arm
-context.beginPath();
-context.arc(420, 180, 20, 0, Math.PI * 2, true);
-context.fill();
-
-
-
-// eyes
-context.beginPath();
-context.arc(288, 90, 5, 0, Math.PI * 2, true);
-context.fillStyle = 'white';
-context.fill();
-context.beginPath();
-context.arc(308, 90, 5, 0, Math.PI * 2, true);
-context.fill();
-
-var img = new Image();   // Create new img element
-img.src = './hacktoberfest.png';
-img.height = "10px"
-const ctx = context
-img.onload = function() {
-    ctx.drawImage(img, 250, 171, 100, 100);
-
-  };
+animate();
